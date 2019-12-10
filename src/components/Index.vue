4da3523f3f73d96000b220aeaf0a7bb9b801cb53
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-12-10 12:09:26 -0500

    added slugify and created addIng and deleteIng methods

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,9 +2,11 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
-        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)"
+          >delete</i
+        >
         <h2 class="indigo-text">
-          {{smoothie.title}}
+          {{ smoothie.title }}
         </h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -12,74 +14,74 @@
           </li>
         </ul>
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
-import db from '@/firebase/init'
+import db from "@/firebase/init";
 export default {
-  name: 'Index',
-  data () {
+  name: "Index",
+  data() {
     return {
-      smoothies: []
-    }
+      smoothies: [],
+    };
   },
   methods: {
-    deleteSmoothie(id){
-      db.collection('smoothies').doc(id).delete()
-        .then(() =>{
-          this.smoothies = this.smoothies.filter(smoothie =>{
-          return smoothie.id != id
-        })
-      })
-    }
+    deleteSmoothie(id) {
+      db.collection("smoothies")
+        .doc(id)
+        .delete()
+        .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id != id;
+          });
+        });
+    },
   },
 
-  created(){
-    db.collection('smoothies').get()
+  created() {
+    db.collection("smoothies")
+      .get()
       .then(snapshot => {
         snapshot.forEach(doc => {
-          let smoothie = doc.data()
-          smoothie.id = doc.id
-          this.smoothies.push(smoothie)
+          let smoothie = doc.data();
+          smoothie.id = doc.id;
+          this.smoothies.push(smoothie);
         });
-      })
-  }
-}
+      });
+  },
+};
 </script>
 
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
 
-<style >
-  .index{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 30px;
-    margin-top: 60px;
-  }
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
 
-  .index h2{
-    font-size: 1.8em;
-    text-align: center;
-    margin-top: 0;
-  }
+.index .ingredients {
+  margin: 30px auto;
+}
 
-  .index .ingredients{
-    margin: 30px auto
-  }
+.index .ingredients li {
+  display: inline-block;
+}
 
-  .index .ingredients li{
-    display: inline-block
-  }
-
-  .index .delete{
-    position: absolute;
-    top: 4px;
-    right: 4px;
-    cursor: pointer;
-    color: #aaa;
-    font-size: 1.4em;
-  }
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
 </style>
